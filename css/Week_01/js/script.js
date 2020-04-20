b36b5773ebd2a78dc676e8e24a3bcbe20a952592
donsonde@gmail.com <donsonde@gmail.com>
2020-04-20 20:14:52 +0000

    firstCommit-20042020

---
+++ css/Week_01/js/script.js
@@ -0,0 +1,5 @@
+//Function to get current year
+document.getElementById('currentYear').innerHTML = new Date().getFullYear();
+
+//Get document last modified date
+document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
